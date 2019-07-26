ab0388468a31d954ecd9ac5ad16af78bd37163be
unknown <unknown@example.com>
2019-07-26 15:26:31 +0800

    7

--- cloudfunctions/customerService/index.js
+++ cloudfunctions/customerService/index.js
@@ -6,12 +6,14 @@ cloud.init()
 // 云函数入口函数
 exports.main = async() => {
     const wxContext = cloud.getWXContext()
-
+    let contentList = ['收到', '嗯', '=￣ω￣=', '(*^_^*)', 'o(*￣▽￣*)ブ'],
+        content;
+    content = contentList[Math.floor(Math.random() * contentList.length)]
     await cloud.openapi.customerServiceMessage.send({
         touser: wxContext.OPENID,
         msgtype: 'text',
         text: {
-            content: '收到',
+            content
         }
     })
 
